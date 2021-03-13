2d9d4b92a8d4f9ad309167fe6c53fa7954c1a6ab
Dilana777 <dylana7792@hotmail.com>
2021-03-13 00:19:10 +0000

    subiendo archivos

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let [frases, setFrase] = useState([
+		"Wash the dishes",
+		"Clean my room",
+		"Cook"
+	]);
+
+	let [tarea, setTarea] = useState("");
+
+	const updatedtoDo = frases.map((listItems, i) => {
+		return (
+			<div key={i}>
+				<li className="li">{listItems}</li>
+				<button onClick={e => deleteToDo(i)}>X</button>
+			</div>
+		);
+	});
+	let lista = (
+		<div>
+			<ul className="list-group m-5  ">{updatedtoDo}</ul>
+		</div>
+	);
+
+	function handleChange(k) {
+		if (k.keyCode === 13) {
+			setFrase([...frases, tarea]);
+			console.log(tarea);
+		}
+	}
+
+	function deleteToDo(i) {
+		frases.splice(i, 1);
+		setFrase([...frases]);
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="cuadro">
+			<h1>Todos</h1>
+			<div sytle="mt-2">
+				<input
+					className="entrada"
+					type="text"
+					onChange={e => setTarea(e.target.value)}
+					onKeyUp={k => handleChange(k)}
+					value={tarea}
+					placeholder="Enter Do"
+					required
+				/>
+				<div className="listas ul">{lista}</div>
+			</div>
+			<section> tienes {frases.length} tareas por completar </section>
 		</div>
 	);
 }
